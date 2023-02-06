1794969736ef66a992df542295c79e70328175f9
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2023-02-06 13:10:01 -0300

    added token verification

--- src/controllers/webhook.ts
+++ src/controllers/webhook.ts
@@ -1,9 +1,21 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { Request, response, Response } from 'express';
 
 const getMessages = async (req: Request, res: Response) => {
   try {
-    console.log(res);
-    res.send(req);
+    const qObject: any = req.query;
+
+    // console.log(qObject);
+    Object.keys(qObject).map((k) => {
+      const newObj = (qObject[k.toString().trim()] = qObject[k]);
+      delete qObject[k];
+      return newObj;
+    });
+
+    if (qObject['hub.verify_token'] === process.env.META_TOKEN) {
+      return res.send(qObject['hub.challenge']);
+    } else res.send('token errone');
   } catch (e) {
     // handleHttp(res, 'ERROR_GET_TRANSACTIONS', e);
   }
