ee967bf46903933dc3a12fb28c3e200bf0668fa3
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2023-02-07 14:13:12 -0300

    changes in types and added other type of events to emit in sIO

--- src/controllers/webhook.ts
+++ src/controllers/webhook.ts
@@ -19,25 +19,10 @@ const getMessages = async (req: Request, res: Response) => {
 
 const notifyWhatsappMessage = async (req: RequestIO, res: Response) => {
   try {
-    if (req?.body?.content?.entry[0]?.changes[0]?.value?.statuses) {
-      req?.io?.emit('message-status-as-change', { content: req.body });
-      console.log('message-status-as-change');
-      return res.send('resOk');
-    } else if (req?.body?.content?.entry[0]?.changes[0]?.value?.messages) {
-      req?.io?.emit('new-message', { content: req.body });
-      console.log('new-message');
-      return res.send('resOk');
-    } else if (req?.body?.content?.entry[0]?.value?.statuses) {
-      req?.io?.emit('message-status-as-change', { content: req.body });
-      console.log('message-status-as-change');
-      return res.send('resOk');
-    } else if (req?.body?.content?.entry[0]?.value?.messages) {
-      req?.io?.emit('new-message', { content: req.body });
-      console.log('new-message');
-      return res.send('resOk');
-    } else {
-      return res.send('resOk');
-    }
+    req?.io?.emit('new-message', { content: req.body });
+    console.log('new-message');
+
+    return res.send('resOk');
   } catch (e) {
     console.log(e);
     return res.send('error');
