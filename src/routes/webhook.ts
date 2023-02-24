ce8dfdb81813e16f009a3479fc54b201657cda1f
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2023-02-24 19:59:09 -0300

    modified webhook direction

--- src/routes/webhook.ts
+++ src/routes/webhook.ts
@@ -1,9 +1,13 @@
 import { Request, Response, Router } from 'express';
 import { getMessages, notifyWhatsappMessage } from '../controllers/webhook';
+import { getMessagesFromMessenger } from '../controllers/messenger';
+
 import { socketMiddleware } from '../middleware/mw';
 const router = Router();
 
 router.get('/whatsapp', getMessages);
+
+router.get('/messenger', getMessagesFromMessenger);
 
 router.post('/whatsapp', socketMiddleware, notifyWhatsappMessage);
 
